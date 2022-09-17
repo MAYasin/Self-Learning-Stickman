34180e0f8cdf2bf209f405b50be03e72eb3e31ac
Muhammad Aman Yasin <68651069+MAYasin@users.noreply.github.com>
2022-09-17 22:28:14 +0200

    route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var sever = app.listen(process.env.PORT || 3000);
 
 app.use(express.static('src'));
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, 'src') });
+})
+
 console.log("sever running");
 
 console.log("http://localhost:3000/");
